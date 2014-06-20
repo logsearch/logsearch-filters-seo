0c6f54c80466d83f76090af0f2d912da8edaff79
Ryan Holder <ryan@ryanholder.com>
2014-06-20 11:35:06 +0100

    Relates to issue #1 splitting charset from content type
        # Convert content_type => "text/xml; charset=utf-8"
        # to
        # content_type => { type =>"text/xml", charset=> "utf-8" }

--- test/googlebot-spec.rb
+++ test/googlebot-spec.rb
@@ -11,8 +11,9 @@ describe LogStash::Filters::Grok do
   CONFIG
 
   describe "Parse Googlebot JSON" do
-    
+
     sample("@type" => "googlebot", "@message" => '{ "@timestamp": "2014-06-07T01:53:14-07:00", "remote_addr": "66.249.67.102", "body_bytes_sent": 9943, "request_time": 0.770, "status": 200, "request_method": "GET", "scheme": "https", "server_name": "yoast.com", "request_uri": "/hire-us/website-review/", "document_uri": "/index.php", "http_user_agent": "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)" }') do
+
       insist { subject["@type"] } == "googlebot"
       insist { subject["@timestamp"] } == Time.iso8601("2014-06-07T08:53:14.000Z")
 
@@ -39,16 +40,38 @@ describe LogStash::Filters::Grok do
       insist { subject["remote_addr_dns"] } == "crawl-66-249-67-102.googlebot.com"
 
     end
- 
+
     sample("@type" => "googlebot", "@message" => '{ "@timestamp": "2014-06-08T00:17:39-07:00", "remote_addr": "66.249.69.125", "body_bytes_sent": 8146, "request_time": 0.832, "status": 200, "request_method": "GET", "scheme": "https", "server_name": "yoast.com", "request_uri": "/cat/wordpress/", "document_uri": "/index.php", "http_user_agent": "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)" }') do
 
       insist { subject["remote_addr"] } == "66.249.69.125"
 
       insist { subject["remote_addr_dns"] } == "crawl-66-249-69-125.googlebot.com"
 
-    end   
+    end
 
-    
+
   end
+
+  describe "Parse Googlebot JSON - Split charset out of content-type header" do
+
+    sample("@type" => "googlebot", "@message" => '{ "content_type": "text/xml; charset=UTF-8", "@timestamp": "2014-06-19T21:54:20-07:00", "remote_addr": "66.249.69.45", "body_bytes_sent": 38704, "request_time": 1.539, "status": 200, "robots": "noindex,follow", "redirect_location": "-", "request_method": "GET", "scheme": "https", "server_name": "yoast.com", "request_uri": "/cat/wordpress/feed/", "document_uri": "/index.php", "http_user_agent": "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)" }') do
+
+      #puts subject["content_type"].inspect
+
+      insist { subject["content_type"]["type"] } == "text/xml"
+      insist { subject["content_type"]["charset"] } == "utf-8"
+
+    end
+
+    sample("@type" => "googlebot", "@message" => '{ "content_type": "text/xml", "@timestamp": "2014-06-19T21:54:20-07:00", "remote_addr": "66.249.69.45", "body_bytes_sent": 38704, "request_time": 1.539, "status": 200, "robots": "noindex,follow", "redirect_location": "-", "request_method": "GET", "scheme": "https", "server_name": "yoast.com", "request_uri": "/cat/wordpress/feed/", "document_uri": "/index.php", "http_user_agent": "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)" }') do
+
+      #puts subject["content_type"].inspect
+
+      insist { subject["content_type"]["type"] } == "text/xml"
+
+    end
+
+  end
+
 
 end
